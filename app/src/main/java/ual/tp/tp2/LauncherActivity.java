fd23e5405a8d05af4a6cb706a455e7b2423dfc81
RMA - a125789 <romain.martinelli@worldline.com>
2016-02-01 15:18:37 +0100

    Init

--- app/src/main/java/ual/tp/tp2/LauncherActivity.java
+++ app/src/main/java/ual/tp/tp2/LauncherActivity.java
@@ -4,16 +4,72 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import java.util.Date;
+
 public class LauncherActivity extends AppCompatActivity {
 
     /**
-     * TP1
+     * TP2
      * 1 - Display the date and update it each second (100ms precision) using a simple Thread, but only when the app is front. Display the number of update iteration below.
+     * 2 - Override dedicated Activity methods to prevent information loss that can occur during its lifecycle
+     * 3 - Refactor your timer computation in a Service
      */
+
+    TextView dateLabel;
+    TextView countLabel;
+    Thread timer;
+    long count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+        dateLabel = (TextView) findViewById(R.id.date);
+        countLabel = (TextView) findViewById(R.id.count);
+        count = 0;
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        timer = new Thread() {
+            @Override
+            public void run() {
+
+                long before = System.currentTimeMillis();
+                boolean stop = false;
+
+                while (!stop) {
+                    long now = System.currentTimeMillis();
+                    count++;
+                    if (now > before + 1000) {
+                        before = now;
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                dateLabel.setText(new Date().toString());
+                                countLabel.setText(count + "");
+
+                            }
+                        });
+                    }
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        stop = true;
+                    }
+                }
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        timer.interrupt();
     }
 }
